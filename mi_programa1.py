472a65d69960aa08cfc9125ec334f3c2849c57b4
dannysm112 <144166033+dannysm112@users.noreply.github.com>
2023-12-03 18:12:07 -0600

    Add files via upload

---
+++ mi_programa1.py
@@ -0,0 +1,11 @@
+# Ejemplo de mi_programa.py
+
+import time
+
+def main():
+    for i in range(1, 6):  # Realizar alguna tarea durante 5 segundos
+        print(f"Ejecutando tarea {i}")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
